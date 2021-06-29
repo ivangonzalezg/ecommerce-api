dca7efd889c34a09486ba51ea0099675eaacd19a
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2021-06-28 22:12:49 -0500

    Request for getting user cart

--- api/cart/controllers/cart.js
+++ api/cart/controllers/cart.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findMe(ctx) {
+    ///const response = await strapi.request("post","/users/me",{}
+    const entities = await strapi.services.cart.find({
+      user: ctx.state.user.id,
+    });
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.cart })
+    );
+  },
+};
