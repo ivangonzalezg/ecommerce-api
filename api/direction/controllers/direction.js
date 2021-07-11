b43bb05660d1a3dad0fb122f9b45f733b51e0fc4
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2021-07-11 01:30:40 -0500

    Route to return user directions

--- api/direction/controllers/direction.js
+++ api/direction/controllers/direction.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async myDirections(ctx) {
+    const entities = await strapi.services.direction.find({
+      user: ctx.state.user.id,
+    });
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.direction })
+    );
+  },
+};
